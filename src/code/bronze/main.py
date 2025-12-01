633fcf3bab45cfb8daee54d4fb9993eb299cd7e7
Felipe Leite <felipe.leite@dataside.com.br>
2025-12-01 10:48:21 -0300

    feat: Add policy new version in glue

--- src/code/bronze/main.py
+++ src/code/bronze/main.py
@@ -12,6 +12,10 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
 
+logger = glueContext.get_logger()
+
+
+
 spark.conf.set("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
 spark.conf.set(
     "spark.sql.catalog.spark_catalog",
@@ -29,54 +33,66 @@ bronze_visitas_path = f"s3://{bucket}/bronze/visitas"
 checkpoint_visitas_path = f"s3://{bucket}/landing/checkpoints/bronze/visitas/"
 schema_visitas_path     = f"s3://{bucket}/landing/schema/bronze/visitas/"
 
-df_pedidos_landing = (
-    spark.read
-    .option("header", "true")
-    .option("inferSchema", "true")
-    .csv(landing_pedidos_path)
-)
+exception_error = ""
 
-df_pedidos_bronze = (
-    df_pedidos_landing
-    .withColumn("valor_total", col("valor_total").cast("double"))
-    .withColumn("data_pedido", col("data_pedido").cast("date"))
-)
+try:
 
-(
-    df_pedidos_bronze.write
-    .format("delta")
-    .mode("overwrite")
-    .option("overwriteSchema", "true")
-    .save(bronze_pedidos_path)
-)
+    df_pedidos_landing = (
+        spark.read
+        .option("header", "true")
+        .option("inferSchema", "true")
+        .csv(landing_pedidos_path)
+    )
+
+    df_pedidos_bronze = (
+        df_pedidos_landing
+        .withColumn("valor_total", col("valor_total").cast("double"))
+        .withColumn("data_pedido", col("data_pedido").cast("date"))
+    )
+
+    (
+        df_pedidos_bronze.write
+        .format("delta")
+        .mode("overwrite")
+        .option("overwriteSchema", "true")
+        .save(bronze_pedidos_path)
+    )
+except Exception as err:
+    logger.error(err)
+    exception_error = err
+
+try:
+    df_visitas_stream = (
+        spark.readStream
+        .format("cloudFiles")
+        .option("cloudFiles.format", "json")
+        .option("cloudFiles.inferColumnTypes", "true")
+        .option("cloudFiles.schemaLocation", schema_visitas_path)
+        .load(landing_visitas_path)
+    )
+
+    df_visitas_bronze = (
+        df_visitas_stream
+        .withColumn("event_time", to_timestamp(col("event_time")))
+        .withColumn("cliente_id", col("cliente_id").cast("int"))
+    )
+
+    query = (
+        df_visitas_bronze.writeStream
+        .format("delta")
+        .option("checkpointLocation", checkpoint_visitas_path)
+        .option("mergeSchema", "true")
+        .outputMode("append")
+        .trigger(availableNow=True)
+        .start(bronze_visitas_path)
+    )
 
 
-df_visitas_stream = (
-    spark.readStream
-    .format("cloudFiles")
-    .option("cloudFiles.format", "json")
-    .option("cloudFiles.inferColumnTypes", "true")
-    .option("cloudFiles.schemaLocation", schema_visitas_path)
-    .load(landing_visitas_path)
-)
-
-df_visitas_bronze = (
-    df_visitas_stream
-    .withColumn("event_time", to_timestamp(col("event_time")))
-    .withColumn("cliente_id", col("cliente_id").cast("int"))
-)
-
-query = (
-    df_visitas_bronze.writeStream
-    .format("delta")
-    .option("checkpointLocation", checkpoint_visitas_path)
-    .option("mergeSchema", "true")
-    .outputMode("append")
-    .trigger(availableNow=True)
-    .start(bronze_visitas_path)
-)
-
-
-query.awaitTermination()
+    query.awaitTermination()
+except Exception as err:
+    logger.error(err)
+    if not exception_error:
+        exception_error = err
+    raise exception_error
 
 job.commit()
