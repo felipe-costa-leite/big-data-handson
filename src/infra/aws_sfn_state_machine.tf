c8b8685f494cf1d4269c33d20839854d6454f732
Felipe Leite <felipe.leite@dataside.com.br>
2025-12-01 15:56:54 -0300

    feat: Add StepFunction for EMR

--- src/infra/aws_sfn_state_machine.tf
+++ src/infra/aws_sfn_state_machine.tf
@@ -5,15 +5,17 @@ resource "aws_sfn_state_machine" "aws-sfn-emr-handson-bigdata" {
   definition = templatefile(
     "${path.module}/files/step_functions_handson_bigdata.json",
     {
-      emr_release_label        = var.emr_release_label
-      emr_log_bucket           = var.emr_log_bucket
-      emr_log_prefix           = var.emr_log_prefix
-      emr_subnet_id            = var.emr_subnet_id
+      emr_cluster_name        = "emr-handson-bigdata"
+      emr_release_label       = var.emr_release_label
+      emr_log_bucket          = var.emr_log_bucket
+      emr_log_prefix          = var.emr_log_prefix
+      emr_subnet_id           = var.emr_subnet_id
       emr_master_instance_type = var.emr_master_instance_type
-      emr_pipeline_script_s3   = var.emr_pipeline_script_s3
-      emr_service_role_name    = aws_iam_role.aws-iam-role-emr-service-profile.name
-      emr_jobflow_role_name    = aws_iam_role.aws-iam-role-emr-instance-profile.name
-      region    = var.region
+      emr_service_role_name   = aws_iam_role.aws-iam-role-emr-service-profile.name
+      emr_jobflow_role_name   = aws_iam_role.aws-iam-role-emr-instance-profile.name
+      bronze_script_path      = "s3://aws-s3-dados-data-lake/artifacts/code/bronze/main.py"
+      silver_script_path      = "s3://aws-s3-dados-data-lake/artifacts/code/silver/main.py"
+      gold_script_path        = "s3://aws-s3-dados-data-lake/artifacts/code/gold/main.py"
     }
   )
 }
